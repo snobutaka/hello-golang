47326302b85c39ebdbf97de9af5c5bf883d14dc8
Nobutaka SAITO <nobutaka.saitoh@gmail.com>
2019-01-06 22:11:43 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Golang!
+
+[How to Write Go Code](https://golang.org/doc/code.html) の内容に沿って書いてみたコード．
---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/snobutaka/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!dlrow olleH"))
+}
